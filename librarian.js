93c891072b4e0dc6b0ef4c6b98a7f933f0fe8d94
zabid01 <zabid.antor@gmail.com>
2018-02-05 09:23:26 +0600

    Lec 7

--- librarian.js
+++ librarian.js
@@ -1,25 +1,28 @@
+
 var library = [
   { title: "Norse Mythology", year: "2017", author: "Neil Gaiman", publisher: "W. W. Norton & Company" },
   { title: "The Old Man and the Sea", year: "1952", author: "Ernest Hemingway", publisher: "Charles Scribner's Sons" },
   { title: "Mythos: A Retelling of the Myths of Ancient Greece", year: "1981", author: "Stephen Fry", publisher: "Penguin" },
   { title: "The Mythical Man-Month", year: "1975", author: "Fred Brooks", publisher: "Addison-Wesley" },
-  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" }
-  /* Task 2 */
+  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" },
+  {title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	"}
+
 ];
 
 /* Task 1 */
 function getBookTitle( index ){
-  //add code
+  return library[index].title;
 }
 
-console.log(getBookTitle(3));
+console.log(getBookTitle(2));
 
-/* Task 3 */
+//* Task 3 *//
 function addBook( book ){
-  //add code
+  library.push(book);
+  return library;
 }
 
-var newBook = {};
+var newBook = {title: "test1 ", year: "test2", author: "test3 ", publisher: "test4	"};
 console.log(addBook(newBook));
 
 /* Task 4 */
